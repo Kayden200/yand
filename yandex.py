fb9d067c688c58f58d294ef775f8588523db3a95
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2025-03-12 21:59:16 +0800

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -74,4 +74,4 @@ def check_otp():
     return jsonify({"otp": otp})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
